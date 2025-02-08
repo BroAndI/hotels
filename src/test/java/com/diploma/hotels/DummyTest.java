aa330f6864bcc376f1a578dcf24f83eb9d906efe
IvanVirchenko <virchenkoivanmail@gmail.com>
2025-02-08 17:57:46 +0200

    set project name for sonar

---
+++ src/test/java/com/diploma/hotels/DummyTest.java
@@ -0,0 +1,13 @@
+package com.diploma.hotels;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DummyTest {
+
+    @Test
+    void dummyTestForSonar() {
+        Assertions.assertTrue("Hello World".length() > 0);
+    }
+
+}
